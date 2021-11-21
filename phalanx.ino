4590549f8913073be283a3b29f402d65747500f0
Danny de Bruijne <dannydb44@gmail.com>
2021-11-21 16:13:26 +0000

    Add an empty option to the dropdowns so not changing it doesn't set it to default.

--- phalanx.ino
+++ phalanx.ino
@@ -26,9 +26,10 @@ bool doLoop = false;
 
 void handleRoot()
 {
-	String hourDropdown = "<option value = \"0\">0</option><option value = \"1\">1</option><option value = \"2\">2</option><option value = \"3\">3</option><option value = \"4\">4</option><option value = \"5\">5</option><option value = \"6\">6</option><option value = \"7\">7</option><option value = \"8\">8</option><option value = \"9\">9</option><option value = \"10\">10</option><option value = \"11\">11</option><option value = \"12\">13</option><option value = \"14\">14</option><option value = \"15\">15</option><option value = \"16\">16</option><option value = \"17\">17</option><option value = \"18\">18</option><option value = \"19\">19</option><option value = \"20\">20</option><option value = \"21\">21</option><option value = \"22\">22</option><option value = \"23\">23</option><option value = \"24\">24</option>";
+	String hourDropdown = "<option value=\"\">-</option><option value = \"0\">0</option><option value = \"1\">1</option><option value = \"2\">2</option><option value = \"3\">3</option><option value = \"4\">4</option><option value = \"5\">5</option><option value = \"6\">6</option><option value = \"7\">7</option><option value = \"8\">8</option><option value = \"9\">9</option><option value = \"10\">10</option><option value = \"11\">11</option><option value = \"12\">13</option><option value = \"14\">14</option><option value = \"15\">15</option><option value = \"16\">16</option><option value = \"17\">17</option><option value = \"18\">18</option><option value = \"19\">19</option><option value = \"20\">20</option><option value = \"21\">21</option><option value = \"22\">22</option><option value = \"23\">23</option><option value = \"24\">24</option>";
 
 	String str = "<!DOCTYPE html><html><head><title>Phalanx</title></head>";
+	str.reserve(3500);	//3.5kb
 	str += "<body><h1>Phalanx Config</h1>";
 	str += "Current Connection Status: ";
 	str += WifiStatusCode[WiFi.status()];
@@ -39,7 +40,7 @@ void handleRoot()
 	str += "\"></br>WiFi Passphrase: <input type=\"text\" name=\"password\" maxLength=32 placeholder=\"WiFi Passphrase\" value=\"";
 	str += String(saveData.wifi_pass);
 
-	str += "\"><br/>Timezone: <select name=\"timezone\"><option value=\"0\">UTC</option><option value=\"1\">Britain, Portugal</option><option value=\"2\">Central Europe</option><option value=\"3\">Moscow</option><option value=\"4\">Australia Eastern</option><option value=\"5\">America Eastern</option></select>";
+	str += "\"><br/>Timezone: <select name=\"timezone\"><option value=\"\">-</option><option value=\"0\">UTC</option><option value=\"1\">Britain, Portugal</option><option value=\"2\">Central Europe</option><option value=\"3\">Moscow</option><option value=\"4\">Australia Eastern</option><option value=\"5\">America Eastern</option></select>";
 
 	str += "</br>12h mode: <input type=\"checkbox\" id=\"time_12hmode\" name=\"time_12hmode\" ";
 	if (saveData.time_12hmode)
@@ -70,22 +71,22 @@ void handleSave()
 	if (webServer.hasArg("password"))
 		strncpy(saveData.wifi_pass, webServer.arg("password").c_str(), 32);
 
-	if (webServer.hasArg("timezone"))
+	if (webServer.hasArg("timezone") && !webServer.arg("timezone").isEmpty())
 		saveData.timeZone = webServer.arg("timezone").toInt();
 
 	saveData.time_12hmode = webServer.hasArg("time_12hmode");
 	saveData.time_displayZeroFirstDigit = webServer.hasArg("time_zerofirstdigit");
 	saveData.time_displaySeconds = webServer.hasArg("time_displaySeconds");
 
-	if (webServer.hasArg("activehrs_begin"))
+	// Active Hours
+	if (webServer.hasArg("activehrs_begin") && !webServer.arg("activehrs_begin").isEmpty())
 		saveData.activeHours[0] = webServer.arg("activehrs_begin").toInt();
 		
-	if (webServer.hasArg("activehrs_end"))
+	if (webServer.hasArg("activehrs_end") && !webServer.arg("activehrs_end").isEmpty())
 		saveData.activeHours[1] = webServer.arg("activehrs_end").toInt();
 
 	if (saveData.activeHours[0] > saveData.activeHours[1])
 		saveData.activeHours[0] = saveData.activeHours[1];
-
 	if (saveData.activeHours[1] < saveData.activeHours[0])
 		saveData.activeHours[1] = saveData.activeHours[0];
 
